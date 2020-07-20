4717bd65d3c904b02484fc5de047dd7c3cab9d9e
siliconninja <theninja@thesilicon.ninja>
2020-07-20 16:29:38 -0400

    Initial stab at writing a test for the Header component

---
+++ src/components/__tests__/header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import Header from "../header"
+
+// const $ = require("jquery")
+
+describe("Header", () => {
+  it("shows the right text", () => {
+    const tree = renderer
+      .create(<Header />)
+      .toJSON()
+    expect(tree).toContain('Messages of')
+  })
+})
